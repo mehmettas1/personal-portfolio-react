cb5729333bcdde1f7010efc6d6a9da84b0815dc9
mehmet <tas349873@gmail.com>
2023-06-10 14:30:17 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import "./App.css";
 
-import './App.css';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/Navbar";
+import { Banner } from "./components/Banner";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Skills } from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
-<NavBar/>
-<Banner/>
+      <NavBar />
+      <Banner />
+      <Skills/>
     </div>
   );
 }

319225e9cfb037087b92571adf4589343f0adfb2
mehmet <tas349873@gmail.com>
2023-06-15 19:43:56 +0300

    update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -35,7 +35,9 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>HTML5 | CSS3 | JavaScript | React.js | Redux | Tailiwind | Alpine.js |Metronic |Drupal| SASS | Bootstrap | Material.UI | Axios | Python |
+Linux | SQLite | Git | GitHub | APIs | Agile | Jira | SDLC | OOP | RESTful APIs.
+</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />

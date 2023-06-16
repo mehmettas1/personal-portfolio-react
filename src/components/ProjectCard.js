1028b0b794f12cc9e72ceb846c9550e004d26c60
mehmet <tas349873@gmail.com>
2023-06-16 17:58:31 +0300

    update

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
+      <div className="proj-imgbx" >
       <img src={imgUrl} style={{height: "300px"}} />
 
         <div className="proj-txtx">
